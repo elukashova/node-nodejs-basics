6061f9e5aa5d3b05cb15f36812bbc87c31592d0c
elukashova <ele.lukashova@gmail.com>
2024-01-24 22:36:39 +0100

    feat: implement cli/env.js subtask

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
-const parseEnv = () => {
-    // Write your code here 
+const prefix = 'RSS_';
+
+const parseEnv = (prefix) => {
+    const output = Object.entries(process.env)
+    .map(([key, value]) => {
+        if (key.startsWith(prefix)) {
+            return `${key}=${value};`;
+        }
+    })
+
+    console.log(...output.filter(Boolean));
 };
 
-parseEnv();
+parseEnv(prefix);
