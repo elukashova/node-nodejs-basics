eb6d04b26f33e3af7b208b07fe9e6230a9163d87
elukashova <ele.lukashova@gmail.com>
2024-01-24 21:39:28 +0100

    feat: implement fs/delete.js subtask

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import * as path from 'node:path';
+import * as fs from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { errorHandler } from '../error-handler.js';
+import { ERROR_CODES } from '../consts.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const targetFolder = 'files';
+const fileToRemove = 'fileToRemove.txt';
+const targetFilePath = path.join(__dirname, targetFolder, fileToRemove);
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await fs.rm(targetFilePath);
+    } catch (error) {
+        errorHandler(error, ERROR_CODES.noSuchFileOrDirectory);
+    } 
 };
 
 await remove();
