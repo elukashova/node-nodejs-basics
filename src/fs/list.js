703236350d5010b6b86865504c5414a3ae628948
elukashova <ele.lukashova@gmail.com>
2024-01-24 21:46:53 +0100

    feat: implement fs/list.js subtask

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import * as path from 'node:path';
+import { readdir } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { errorHandler } from '../error-handler.js';
+import { ERROR_CODES } from '../consts.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const targetFolder = 'files';
+const targetFolderPath = path.join(__dirname, targetFolder);
+
 const list = async () => {
-    // Write your code here 
+    let fileNames;
+    try {
+        fileNames = await readdir(targetFolderPath);
+    } catch (error) {
+        errorHandler(error, ERROR_CODES.noSuchFileOrDirectory);
+    }
+
+    console.log(fileNames);
 };
 
 await list();
