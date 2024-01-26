64c9ddbc841d9c5c8638003779cc2d6d7e90f873
elukashova <ele.lukashova@gmail.com>
2024-01-26 22:46:19 +0100

    feat: implement cli/args.js subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,22 @@
-const parseArgs = () => {
-    // Write your code here 
+const prefix = '--';
+
+const parseArgs = (prefix) => {
+    const args = process.argv.slice(2);
+
+    const processedData = args.reduce((acc, arg) => {
+        if (arg.startsWith(prefix)) {
+            const key = [arg.slice(2)];
+            acc.push(key);
+        } else {
+            const pair = acc.at(-1);
+            pair?.push(arg);
+        }
+        return acc;
+    }, [])
+
+    const output = processedData.map(([key, value]) => `${key} is ${value}`).join(', ');
+
+    console.log(output);
 };
 
-parseArgs();
+parseArgs(prefix);
