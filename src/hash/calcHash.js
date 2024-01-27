a68674925d3c8a4b5c4a0502de5eea77b018864d
elukashova <ele.lukashova@gmail.com>
2024-01-27 21:44:52 +0100

    feat: implement hash/calcHash.js subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
-const calculateHash = async () => {
-    // Write your code here 
+import * as crypto from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { getPath } from '../utils.js';
+
+const sourceFileName = 'fileToCalculateHashFor.txt';
+const sourceFilePath = getPath(fileURLToPath(import.meta.url), sourceFileName);
+const calculateHash = async (sourcePath) => {
+    const hash = crypto.createHash('sha256').setEncoding('hex');
+    createReadStream(sourcePath)
+    .pipe(hash)
+    .pipe(process.stdout);
 };
 
-await calculateHash();
+await calculateHash(sourceFilePath);
