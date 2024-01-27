ef69b91683549e7d7dbdc5afe81824efdce8fcf1
elukashova <ele.lukashova@gmail.com>
2024-01-27 21:54:49 +0100

    feat: implement streams/read.js subtask

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
-const read = async () => {
-    // Write your code here 
+import { createReadStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { getPath } from '../utils.js';
+
+const fileToRead = 'fileToRead.txt';
+const fileToReadPath = getPath(fileURLToPath(import.meta.url), fileToRead);
+
+const read = async (filePath) => {
+    createReadStream(filePath).pipe(process.stdout);
 };
 
-await read();
+await read(fileToReadPath);
