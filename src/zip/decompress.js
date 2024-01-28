2e6d1c947f6b734930b38d7bf42dc91eff33ab33
elukashova <ele.lukashova@gmail.com>
2024-01-28 12:03:33 +0100

    feat: implement zip/decompress.js subtask

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
-const decompress = async () => {
-    // Write your code here 
+import { createReadStream, createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { createGunzip } from 'node:zlib';
+import { getPath } from '../utils.js';
+
+const fileToDecompress = 'archive.gz';
+const targetFile = 'fileToCompress.txt';
+const fileToDecompressPath = getPath(fileURLToPath(import.meta.url), fileToDecompress);
+const targetFilePath = getPath(fileURLToPath(import.meta.url), targetFile);
+const decompress = async (source, target) => {
+    const gunzip = createGunzip();
+    const readStream = createReadStream(source);
+    const writeStream = createWriteStream(target);
+  
+    readStream.pipe(gunzip).pipe(writeStream);
 };
 
-await decompress();
+await decompress(fileToDecompressPath, targetFilePath);
