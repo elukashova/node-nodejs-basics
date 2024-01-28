22614184262acd7bf0d48856f8daa1c41d90089e
elukashova <ele.lukashova@gmail.com>
2024-01-28 11:07:30 +0100

    feat: implement streams/write.js subtask

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
-const write = async () => {
-    // Write your code here 
+import { createWriteStream } from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import { getPath } from '../utils.js';
+
+const fileToWrite = 'fileToWrite.txt';
+const fileToWritePath = getPath(fileURLToPath(import.meta.url), fileToWrite);
+const write = async (filePath) => {
+    const writeStream = createWriteStream(filePath);
+    process.stdin.pipe(writeStream);
 };
 
-await write();
+await write(fileToWritePath);
