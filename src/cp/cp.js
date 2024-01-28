a0bbc4601343af2c72b4a16656ff1a38390a6732
elukashova <ele.lukashova@gmail.com>
2024-01-28 16:17:40 +0100

    feat: implement cp/cpj.js subtask

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from 'node:child_process';
+import { fileURLToPath } from 'node:url';
+import { getPath } from '../utils.js';
+
+const sourceFile = 'script.js';
+const sourceFilePath = getPath(fileURLToPath(import.meta.url), sourceFile);
+
+const spawnChildProcess = async (filePath, args) => {
+    const argsToPass = args ?? [];
+    spawn('node', [filePath, ...argsToPass], {
+        stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( sourceFilePath, /* [someArgument1, someArgument2, ...] */);
